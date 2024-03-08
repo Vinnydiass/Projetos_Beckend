f34c9956d1f102cb16fd8d538ee31a919d453064
Vinnydiass <vinynavidareal@gmail.com>
2024-03-07 21:51:27 -0300

    GFDG

--- Construção De BeckAnd/ListaExercicios/exercicios1/exercicios.js
+++ Construção De BeckAnd/ListaExercicios/exercicios1/exercicios.js
@@ -3,3 +3,24 @@ let prompt = require('prompt-sync')()
 let nome = prompt("Qual o seu nome?")
 
 console.log("Olá " + nome + " Bem-Vindo")
+
+let idade = prompt("Qual a sua idade? ")
+
+if(idade >= 18){
+    console.log("Você é maior de idade!")
+} else {
+    console.log("Você é menor de idade!")
+}
+
+let notaProva1 = Number(prompt("Nota prova A1? "))
+let notaProva2 = Number(prompt("Nota prova A2? "))
+
+let media = (notaProva1 + notaProva2) / 2
+
+console.log("Media: ", media)
+
+if(media >= 6){
+    console.log("Aprovado")
+} else {
+    console.log("Reprovado viadinho burro")
+}
--- Construção De BeckAnd/ListaExercicios/exercicios1/tempCodeRunnerFile.js
+++ Construção De BeckAnd/ListaExercicios/exercicios1/tempCodeRunnerFile.js
@@ -3,3 +3,22 @@ let prompt = require('prompt-sync')()
 let nome = prompt("Qual o seu nome?")
 
 console.log("Olá " + nome + " Bem-Vindo")
+
+let idade = prompt("Qual a sua idade? ")
+
+if(idade >= 18){
+    console.log("Você é maior de idade!")
+} else {
+    console.log("Você é menor de idade!")
+}
+
+let notaProva1 = prompt("Nota prova A1? ")
+let notaProva2 = prompt("Nota prova A2? ")
+
+let media = (notaProva1 + notaProva2) / 2
+
+if(media >= 6){
+    console.log("Aprovado")
+} else {
+    console.log("Reprovado viadinho burro")
+}
